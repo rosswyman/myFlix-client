4bc018c93c103cfb2ccc75f5c313410d0a8fb1af
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-06-28 14:46:15 -0500

    Update main-view.jsx
    Added data to in-memory array of movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,9 +8,9 @@ export class MainView extends React.Component{
     super();
     this.state={
       movies:[
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
+        { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', ImagePath: 'https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/', Genre: 'Thriller', Director: 'Christopher Nolan'},
+        { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/', Genre: 'Thriller', Director: 'Frank Darabont'},
+        { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath: 'https://www.imdb.com/title/tt0172495/mediaviewer/rm2442542592/',Genre: 'Action', Director: 'Ridley Scott'}
       ],
       selectedMovie: null
     };
