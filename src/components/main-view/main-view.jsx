b1f04b71cfbbbc4de06ee21d8dd68e9494b99c8d
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-06-29 12:10:48 -0500

    Update main-view.jsx
    Connected client to heroku web api

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,36 +9,49 @@ export class MainView extends React.Component{
   constructor(){
     super();
     this.state={
-      movies:[
-        { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', ImagePath: 'https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/', Genre: 'Thriller', Director: 'Christopher Nolan'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/', Genre: 'Thriller', Director: 'Frank Darabont'},
-        { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath: 'https://www.imdb.com/title/tt0172495/mediaviewer/rm2442542592/',Genre: 'Action', Director: 'Ridley Scott'}
-      ],
+      // movies:[
+      //   { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', ImagePath: 'https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/', Genre: 'Thriller', Director: 'Christopher Nolan'},
+      //   { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/', Genre: 'Thriller', Director: 'Frank Darabont'},
+      //   { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath: 'https://www.imdb.com/title/tt0172495/mediaviewer/rm2442542592/',Genre: 'Action', Director: 'Ridley Scott'}
+      // ],
+      movies:[],
       selectedMovie: null
     };
   }
 
-setSelectedMovie(newSelectedMovie){
-  this.setState({
-    selectedMovie: newSelectedMovie
-  });
-}
+  componentDidMount(){
+    axios.get('https://movieboom.herokuapp.com/movies')
+    .then(response=>{
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  setSelectedMovie(newSelectedMovie){
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
+  }
 
   render(){
     const{movies, selectedMovie}=this.state // This is an example of object destruction
       
-    if (movies.length===0) return <div className="main-view">The list is empty</div>;
+    if (movies.length===0) return <div className="main-view" />;
 
     return (
     <div className="main-view">
       {selectedMovie
         ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
         : movies.map(movie => (
-          <MovieCard key={movie._id} movieData={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+          <MovieCard key={movie._id} movieData={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
         ))
       }
     </div>
    
     );
-}
+  }
 }
