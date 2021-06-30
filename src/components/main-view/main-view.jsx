83e41bdbeb29460c64e2146b9953a53e71aaba90
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-06-30 10:46:23 -0500

    Update main-view.jsx
    added user to const declaration.  Previously was trying to call user without defining it.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -45,7 +45,7 @@ export class MainView extends React.Component{
     }
 
   render(){
-    const{movies, selectedMovie}=this.state // This is an example of object destruction
+    const{movies, user, selectedMovie}=this.state // This is an example of object destruction
     
   //  If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView
 
