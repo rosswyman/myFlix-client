d04369ff62c19a0c307d051166c52147409a8c75
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-06-30 10:47:11 -0500

    Update registration-view.jsx
    Initial attempt at registration view

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+export function RegistrationView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ email, setPassword ] = useState('');
+  const [ birthday, setPassword ] = useState('');
+
+  const handleSubmit = () => {
+    // e.preventDefault();
+    console.log(username, password, email, birthday);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onLoggedIn(username);
+  };
+
+  return (
+    <form>
+      <h1>New User Registration</h1>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Birthday:
+        <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
+      </label>
+      <button type="button" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
