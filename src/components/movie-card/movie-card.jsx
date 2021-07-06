bdaff93b773f4066ac1dcc499c106143cfdd0de8
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-07-06 12:57:50 -0500

    Update movie-card.jsx
    Updated to include title and description

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
   render() {
     const { movieData, onMovieClick } = this.props;
 
     return (
-      <div onClick={() => onMovieClick(movieData)} className="movie-card">{movieData.Title}</div>
+      <Card>
+        <Card.Img variant="top" src={movieData.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movieData.Title }</Card.Title>
+          <Card.Text>{movieData.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movieData)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
+      
     );
   }
 }
