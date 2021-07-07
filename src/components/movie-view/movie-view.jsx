a3aab5922fca2948c73c1e71382d7efd611a1dfd
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-07-06 20:03:55 -0500

    Update movie-view.jsx
    Alignment changes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import './movie-view.scss'
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
 export class MovieView extends React.Component{
  
 
@@ -7,28 +13,33 @@ export class MovieView extends React.Component{
     const{ movie, onBackClick }=this.props;
     
     return (
-    <div className="movie-view">
-      <div className="movie-poster">
-        <img src={movie.ImagePath} />
-      </div>
-      <div className="movie-title">
-        <span className="label">Title: </span>
-        <span className="value">{movie.Title}</span>
-      </div>
-      <div className="movie-description">
-        <span className="label">Description: </span>
-        <span className="value">{movie.Description}</span>
-      </div>
-      <div className="movie-genere">
-        <span className="label">Genre: </span>
-        <span className="value">{movie.Genre.Name}</span>
-      </div>
-      <div className="movie-director">
-        <span className="label">Director: </span>
-        <span className="value">{movie.Director.Name}</span>
-      </div>
-       <button onClick={()=>{onBackClick(null);}}>Back</button>
-    </div>
+
+      <Row className="movie-view justify-content-md-center">
+        <Col md={8}>
+          <div className="movie-poster">
+            <img src={movie.ImagePath} />
+          </div>
+          <div className="movie-title">
+            <span className="label">Title: </span>
+            <span className="value">{movie.Title}</span>
+          </div>
+          <div className="movie-description">
+            <span className="label">Description: </span>
+            <span className="value">{movie.Description}</span>
+          </div>
+          <div className="movie-genere">
+            <span className="label">Genre: </span>
+            <span className="value">{movie.Genre.Name}</span>
+          </div>
+          <div className="movie-director">
+            <span className="label">Director: </span>
+            <span className="value">{movie.Director.Name}</span>
+          </div>
+          <div className="text-center">
+            <Button onClick={()=>{onBackClick(null);}} variant="outline-primary">Back</Button>
+          </div>
+        </Col>   
+      </Row>
     );  
 }
 }
