a2d2f0b10a847877d1ca7fd2f5c4d98dffec36b9
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-07-12 13:41:44 -0500

    Update movie-card.jsx
    Attempted to add addmovie method to movie card to allow user to add movie to favorites

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import './movie-card.scss';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
@@ -8,6 +10,25 @@ import { Link } from "react-router-dom";
 
 
 export class MovieCard extends React.Component {
+
+addFavorite(movie) {
+  const token = localStorage.getItem("token");
+  const url =
+    "https://movieboom.herokuapp.com/users/" +
+    localStorage.getItem("user") +
+    "/movies/" +
+    movie._id;
+  axios
+    .post(url, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((response) => {
+      console.log(response);
+      this.componentDidMount();
+        alert(movie.Title + " has been added to your Favorites.");
+    });
+  }
+
   render() {
     const { movieData } = this.props;
 
@@ -22,7 +43,10 @@ export class MovieCard extends React.Component {
             <Link to={`/movies/${movieData._id}`}>
               <Button variant="outline-info">Details</Button>
             </Link>
-          </div>          
+          </div>
+          <div>
+          <Button variant="outline-info" onClick={()=>this.addFavorite(movieData)}>Add to Favorites</Button>
+              </div>          
         </Card.Body>
       </Card>
     
