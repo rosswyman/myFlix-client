dd149433ff5d74e2390057fbfe930664f2b25644
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-07-12 15:00:30 -0500

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import './profile-view.scss'
 import { MovieCard } from '../movie-card/movie-card';
 import { FavoriteCard } from '../favorite-card/favorite-card';
-import { Link } from "react-router-dom";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
@@ -62,54 +61,52 @@ removeFavorite(movie) {
     .then((response) => {
       console.log(response);
       this.componentDidMount();
-      // location.reload();
+
       alert(movie.Title + " has been removed from your Favorites.");
     });
   }
 
-  updateUser() {
-    const token = localStorage.getItem("token");
-    const url =
-      "https://movieboom.herokuapp.com/users/" +
-      localStorage.getItem("user");
-    axios
-      .put(url, 
-        {
-          Username: this.state.Username,
-          Password: this.state.Password,
-          Email: this.state.Email,
-          Birthdate: this.state.Birthdate,
-        },
-        {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((response) => {
-        console.log(response);
-        this.componentDidMount();
-          alert(movie.Title + " has been removed from your Favorites.");
-      });
-    }
+  
 
   render(){
-    const{  movies, onBackClick }=this.props;
+    const{  movies, token, onBackClick }=this.props;
     const {loading, username, password, email, birthday, favoriteMovies} = this.state;
     const favoriteMovieList = movies.filter((movie) => {
-
       return this.state.favoriteMovies.includes(movie._id);
     });
 
-    console.log(favoriteMovieList)
-    
-    
-    
-    console.log('Favorite movies: '+favoriteMovieList)
+    const newUsername = document.getElementById('formUsername');
+
+
+    function updateUser(token) {
+  
+      const url = "https://movieboom.herokuapp.com/users/" + localStorage.getItem("user");
+
+
+      axios
+        .put(url, 
+          {
+            Username: newUsername,
+            Password: password,
+            Email: email,
+            Birthdate: birthday
+          },
+          {
+          headers: {'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
+        })
+        .then((response) => {
+          console.log(response);
+          this.componentDidMount();
+            alert("User information has been updated");
+        });
+      }
+
     if (loading) return '<div>Loading...</div>';
     return (
       <Row className="profile-view justify-content-md-center">
-        <Col md={8}>
+        <Col md={10}>
           <div className="user-username">
             <span className="label">Username: </span>
-            {/* <span className="value">{user.Username}</span> */}
             <span className="value">{username}</span>
           </div>
           {/* <div className="user-password">
@@ -123,54 +120,45 @@ removeFavorite(movie) {
           <div className="user-birthday">
             <span className="label">Birthday: </span>
             <span className="value">{birthday}</span>
-          </div>
-          
+          </div> 
           <div className="user-favorites">
-            <span className="label">Favorite Movies: </span>
-            {/* <span className="value">{favoriteMovies }</span> */}
-            </div>
-            </Col>
-            <Row>
+            <span className="label">Favorite Movies: </span>               
             {favoriteMovieList.map((m)=> {
-              return <Col md={3} key={m._id}>
-              {/* <MovieCard movieData={m} /> */}
+              return <Col md={8} key={m._id}>              
               <FavoriteCard movieData={m} user={username} />
              </Col>
             })}
-            </Row>
            
+            </div>
+            </Col>
           
           <div className="text-center">
             <Button onClick={()=>{onBackClick(null);}} variant="outline-info">Back</Button>
           </div>
           <div>
           <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label className="label">Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="formPassword">
-        <Form.Label className="label">Password:</Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="formEmail">
-        <Form.Label className="label">Email:</Form.Label>
-        <Form.Control type="email" onChange={e => setEmail(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="formBirthday">
-        <Form.Label className="label">Username:</Form.Label>
-        <Form.Control type="date" onChange={e => setBirthday(e.target.value)} />
-      </Form.Group>
-
-      
-      <div className="text-center">
-        {/* <Button variant="primary" type="submit" onClick={handleSubmit}> */}
-        <Button variant="primary" type="submit" onClick={()=>this.updateUser}>
-          Submit
-        </Button>
-      </div>
-        
-    </Form>
+            <Form.Group controlId="formUsername">
+              <Form.Label className="label">Username:</Form.Label>
+              <Form.Control type="text" defaultValue={this.state.username}/>
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label className="label">Password:</Form.Label>
+              <Form.Control type="password" defaultValue={this.state.password}/>
+            </Form.Group>
+            <Form.Group controlId="formEmail">
+              <Form.Label className="label">Email:</Form.Label>
+              <Form.Control type="email" defaultValue={this.state.email}/>
+            </Form.Group>
+            <Form.Group controlId="formBirthday">
+              <Form.Label className="label">Birthday:</Form.Label>
+              <Form.Control type="date" defaultValue={this.state.birthday}/>
+            </Form.Group>
+              <div className="text-center">              
+              <Button variant="primary" type="submit" onClick={()=>{updateUser(token)}}>
+              Update User
+              </Button>
+            </div>  
+          </Form>
           </div>
           
       </Row>
