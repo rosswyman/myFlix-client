b55bdeea74b9836b25b3f43eb0c9f7d386194811
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-07-12 20:09:44 -0500

    Update profile-view.jsx
    Cleaned up code for readability

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -66,8 +66,6 @@ removeFavorite(movie) {
     });
   }
 
-  
-
   render(){
     const{  movies, token, onBackClick }=this.props;
     const {loading, username, password, email, birthday, favoriteMovies} = this.state;
@@ -81,7 +79,6 @@ removeFavorite(movie) {
     function updateUser(token) {
   
       const url = "https://movieboom.herokuapp.com/users/" + localStorage.getItem("user");
-
 
       axios
         .put(url, 
@@ -103,64 +100,69 @@ removeFavorite(movie) {
 
     if (loading) return '<div>Loading...</div>';
     return (
-      <Row className="profile-view justify-content-md-center">
-        <Col md={10}>
+      <Row className="profile-view justify-content-md-center">       
+        <Col>
           <div className="user-username">
             <span className="label">Username: </span>
             <span className="value">{username}</span>
           </div>
-          {/* <div className="user-password">
-            <span className="label">Current Password: </span>
-            <span className="value">{password}</span>
-          </div> */}
-          <div className="user-email">
-            <span className="label">E-mail: </span>
-            <span className="value">{email}</span>
-          </div>
-          <div className="user-birthday">
-            <span className="label">Birthday: </span>
-            <span className="value">{birthday}</span>
-          </div> 
-          <div className="user-favorites">
-            <span className="label">Favorite Movies: </span>               
-            {favoriteMovieList.map((m)=> {
-              return <Col md={8} key={m._id}>              
-              <FavoriteCard movieData={m} user={username} />
-             </Col>
-            })}
-           
-            </div>
-            </Col>
-          
-          <div className="text-center">
-            <Button onClick={()=>{onBackClick(null);}} variant="outline-info">Back</Button>
-          </div>
-          <div>
+
+        {/* <div className="user-password">
+          <span className="label">Current Password: </span>
+          <span className="value">{password}</span>
+        </div> */}
+
+        <div className="user-email">
+          <span className="label">E-mail: </span>
+          <span className="value">{email}</span>
+        </div>
+
+        <div className="user-birthday">
+          <span className="label">Birthday: </span>
+          <span className="value">{birthday}</span>
+        </div> 
+
+        <div className="user-favorites">
+          <span className="label">Favorite Movies: </span>               
+          {favoriteMovieList.map((m)=> {
+            return <Col md={3} key={m._id}>              
+          <FavoriteCard movieData={m} user={username} />
+          </Col>
+          })}
+        </div>
+
+        <div className="text-center">
+        <Button on  Click={()=>{onBackClick(null);}} variant="outline-info">Back</Button>
+        </div>
+
+        <div>
           <Form>
             <Form.Group controlId="formUsername">
               <Form.Label className="label">Username:</Form.Label>
               <Form.Control type="text" defaultValue={this.state.username}/>
             </Form.Group>
+
             <Form.Group controlId="formPassword">
               <Form.Label className="label">Password:</Form.Label>
               <Form.Control type="password" defaultValue={this.state.password}/>
             </Form.Group>
+
             <Form.Group controlId="formEmail">
               <Form.Label className="label">Email:</Form.Label>
               <Form.Control type="email" defaultValue={this.state.email}/>
             </Form.Group>
+
             <Form.Group controlId="formBirthday">
               <Form.Label className="label">Birthday:</Form.Label>
               <Form.Control type="date" defaultValue={this.state.birthday}/>
             </Form.Group>
-              <div className="text-center">              
-              <Button variant="primary" type="submit" onClick={()=>{updateUser(token)}}>
-              Update User
-              </Button>
+
+            <div className="text-center">              
+              <Button variant="primary" type="submit" onClick={()=>{updateUser(token)}}>Update User</Button>
             </div>  
           </Form>
-          </div>
-          
+        </div>
+        </Col>
       </Row>
     );  
 }
