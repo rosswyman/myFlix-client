d0323309405683541619082e8ff3f600ac15a106
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-07-14 14:35:50 -0500

    Update profile-view.jsx
    Code cleanup

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -23,16 +23,11 @@ export class ProfileView extends React.Component{
       newPassword: null,
       newEmail: null
     };
-
  
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
- 
+  } 
 
   handleSubmit(event) {
-    
-    
     console.log('Current username: ' + this.state.username);
     console.log('Current password: ' + this.state.password);
     console.log('Current email: ' + this.state.email);
@@ -67,10 +62,10 @@ export class ProfileView extends React.Component{
       localStorage.setItem("user", data.Username);
       this.setState({user: response.data.Username})
       this.getUser(token)
+      window.open('/users/'+localStorage.getItem("user"), '_self');
     })
     .catch(function (error) {
-      console.log(error);
-      
+      console.log(error);      
     });
 
     event.preventDefault();
@@ -172,122 +167,107 @@ export class ProfileView extends React.Component{
   render(){
     const{  movies, token, onBackClick }=this.props;
     const {loading, username, password, email, birthday} = this.state;
-     
 
     // This takes the list of all movies and filters so that only those in the user favorites show up
     const favoriteMovieList = movies.filter((movie) => {
       return this.state.favoriteMovies.includes(movie._id);
-    });
-
-   
+    });  
 
     if (loading) return '<div>Loading...</div>';
 
     return (
       <Row className="profile-view justify-content-md-center">       
         <Col>
-        <h1>Welcome {username}</h1>
-          <div className="user-username">
-            <Row>
-              <Col xs={3}>
-              <span className="label">Username: </span>
-              </Col>
-              <Col>
-              <span className="value">{username}</span>
-              </Col>  
-            </Row>            
-          </div>
-        <div className="user-email">
-          <Row>
+          <h1>Welcome {username}</h1>
+
+          <Row className="user-username">
+            <Col xs={3}>
+            <span className="label">Username: </span>
+            </Col>
+            <Col>
+            <span className="value">{username}</span>
+            </Col>  
+          </Row>
+
+          <Row className="user-email">
             <Col xs={3}>
               <span className="label">E-mail: </span>
             </Col>
             <Col>
               <span className="value">{email}</span>
             </Col>              
-          </Row>          
-        </div>
-        
-        <div className="user-birthday">
-          <Row>
+          </Row>
+
+          <Row className="user-birthday">
             <Col xs={3}>
               <span className="label">Birthday: </span>
             </Col>
             <Col>
               <span className="value">{birthday}</span>
-            </Col>    
-              
+            </Col>
           </Row>
-          
-        </div> 
 
-        <div className="user-favorites">
-          <Row>
+          <Row className="user-favorites">
             <Col xs={3}>
               <span className="label">Favorite Movies: </span>
             </Col>
             <Col>
-            </Col>
-          
+            </Col>          
           </Row>
+
           <Row>               
-          {favoriteMovieList.map((m)=> {
-            return <Col md={3} key={m._id}>              
-          <FavoriteCard movieData={m} user={username} />
-          <div className="text-center">
-          <Button variant="outline-info" onClick={()=>this.removeFavorite(m)}>Remove from Favorites</Button>
-          </div>
-          </Col>
-          })}
+            {favoriteMovieList.map((m)=> {
+              return <Col md={3} key={m._id}>              
+              <FavoriteCard movieData={m} user={username} />
+              <div className="text-center">
+                <Button variant="outline-info" onClick={()=>this.removeFavorite(m)}>Remove from Favorites</Button>
+              </div>
+            </Col>
+            })}
           </Row>
-        
-        </div>
-        <Row>
-          <Col className="back-button-col">
-          <div className="text-center">
-            <Button on  Click={()=>{onBackClick(null);}} variant="outline-info">Back</Button>
-          </div>
-          </Col>
-          
-        </Row>
-        
 
-      
-        <Form className="updated-user-info">
-         <h3 style={{ textAlign: "center" }}>Update User Profile</h3>
-          <Form.Group controlId="formNewUsername">
-            <Form.Label className="label">Username:</Form.Label>
-            <Form.Control type="text" name="username" onChange={(event) => this.handleChange(event)} placeholder="Update username"/>
-          </Form.Group>
-          
-          <Form.Group controlId="formNewPassword">
-            <Form.Label className="label">Password:</Form.Label>
-            <Form.Control type="password" name="password" onChange={(event) => this.handleChange(event)} placeholder="Update password" />
-          </Form.Group>
-          
-          <Form.Group controlId="formNewEmail">
-            <Form.Label className="label">Email:</Form.Label>
-            <Form.Control type="email" name="email" onChange={(event) => this.handleChange(event)} placeholder="Update email" />
-          </Form.Group>
+          <Row>
+            <Col className="back-button-col">
+            <div className="text-center">
+              <Button on  Click={()=>{onBackClick(null);}} variant="outline-info">Back</Button>
+            </div>
+            </Col>
+          </Row>
 
-          <Form.Group controlId="formNewBirthday">
-            <Form.Label className="label">Birthday:</Form.Label>
-            <Form.Control type="date" name="birthday" onChange={(event) => this.handleChange(event)} placeholder="Update birthday" value={birthday} />
-          </Form.Group>
+          <Form className="updated-user-info">
+          <h3 style={{ textAlign: "center" }}>Update User Profile</h3>
+            <Form.Group controlId="formNewUsername">
+              <Form.Label className="label">Username:</Form.Label>
+              <Form.Control type="text" name="username" onChange={(event) => this.handleChange(event)} placeholder="Update username"/>
+            </Form.Group>
+            
+            <Form.Group controlId="formNewPassword">
+              <Form.Label className="label">Password:</Form.Label>
+              <Form.Control type="password" name="password" onChange={(event) => this.handleChange(event)} placeholder="Update password" />
+            </Form.Group>
+            
+            <Form.Group controlId="formNewEmail">
+              <Form.Label className="label">Email:</Form.Label>
+              <Form.Control type="email" name="email" onChange={(event) => this.handleChange(event)} placeholder="Update email" />
+            </Form.Group>
 
-            <div className="text-center">              
-       
-              <Row>
-                <Col>
-                  <Button variant="primary" type="submit" onClick={this.handleSubmit}>Update User Info</Button>
-                </Col>
-                <Col>
-                  <Button variant="danger" onClick={this.deleteUser}>Delete User</Button>
-                </Col>
-              </Row>
-            </div>  
-          
-        </Form>
+            <Form.Group controlId="formNewBirthday">
+              <Form.Label className="label">Birthday:</Form.Label>
+              <Form.Control type="date" name="birthday" onChange={(event) => this.handleChange(event)} placeholder="Update birthday" />
+            </Form.Group>
+
+              <div className="text-center">
+                <Row>
+                  <Col>
+                    <Button variant="primary" type="submit" onClick={this.handleSubmit}>Update User Info</Button>
+                  </Col>
+                  <Col>
+                    <Button variant="danger" onClick={this.deleteUser}>Delete User</Button>
+                  </Col>
+                </Row>
+              </div>  
+            
+          </Form>
         </Col>
       </Row>
     );  
