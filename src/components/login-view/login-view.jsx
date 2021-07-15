d3fa55b72110a91e5f33ee79d533da2769fa9c9a
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-07-15 12:15:49 -0500

    Update login-view.jsx
    Formatting indents

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -24,56 +24,47 @@ export function LoginView(props) {
       Password: password
     })
     .then(response => {
-      const data = response.data;
+      const data = response.data; // data is the user object from the users collection
+      console.log(data)
       props.onLoggedIn(data);
     })
-    .catch(e => {
-      
-      console.log(e)
+    .catch(e => {      
+      console.log(e)      
     });
   };
 
-
-
   return (
     <>
-    <Row>
-      <Col></Col>
-      <Col xs={6}>
-        <Form>
-          <Form.Group controlId="formUsername">
-            <Form.Label className="label">Username:</Form.Label>
-            <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-          </Form.Group>
+      <Row>
+        <Col></Col>
+        <Col xs={6}>
+          <Form>
+            <Form.Group controlId="formUsername">
+              <Form.Label className="label">Username:</Form.Label>
+              <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
 
-          <Form.Group controlId="formPassword">
-            <Form.Label className="label">Password:</Form.Label>
-            <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-          </Form.Group>
-        </Form>
-      </Col>
-      <Col></Col>
-    </Row>
-    <Row>
-      <Col className="text-center">
-        <Button variant="primary" type="submit" onClick={handleSubmit}>Log In</Button>
-      </Col>
-      <Col className="text-center">
-        <Link to={'/register'}>
-          <Button variant="outline-info">Register a New User</Button>
-        </Link>
-      </Col>
-    </Row>
-       
-        
+            <Form.Group controlId="formPassword">
+              <Form.Label className="label">Password:</Form.Label>
+              <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+              <Form.Text className="text-muted">
+                Please re-enter or change password
+              </Form.Text>
+            </Form.Group>
+          </Form>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
+          <Button variant="primary" type="submit" onClick={handleSubmit}>Log In</Button>
+        </Col>
+        <Col className="text-center">
+          <Link to={'/register'}>
+            <Button variant="outline-info">Register a New User</Button>
+          </Link>
+        </Col>
+      </Row>
     </>
-      
-      
-      
-
-    
-    
-        
-   
   );
 }
