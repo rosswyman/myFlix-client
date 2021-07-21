5e9aca45135ab660ed1266fe2e1fdf086f95e26f
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-07-21 09:53:54 -0500

    Update index.jsx
    Updated per exercise instructions

--- src/index.jsx
+++ src/index.jsx
@@ -5,16 +5,18 @@ import Container from 'react-bootstrap/Container';
 
 import { createStore } from 'redux';
 
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
 import { Provider } from 'react-redux';
 
 import moviesApp from './reducers/reducers';
 
-import { MainView } from './components/main-view/main-view';
+import MainView from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
-const store=createStore(moviesApp);
+const store=createStore(moviesApp, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
