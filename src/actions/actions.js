395de3316176dc4c7dc2059636ad3c3822625510
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-07-21 09:52:55 -0500

    Update actions.js
    Adding logs to help development

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,9 +2,11 @@ export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 
 export function setMovies(value) {
+	console.log('setMovies action called');
 	return { type: SET_MOVIES, value };
 }
 
 export function setFilter(value) {
+	console.log('setFilter action called');
 	return { type: SET_FILTER, value };
 }
