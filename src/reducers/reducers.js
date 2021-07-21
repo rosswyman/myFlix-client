63c68bdc3514aa95bb5981a65466fff41b94830c
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-07-21 13:49:22 -0500

    Update reducers.js
    Added setUser

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import { SET_FILTER, SET_MOVIES } from '../actions/actions';
+import { SET_MOVIES, SET_FILTER, SET_USER } from '../actions/actions';
 
 function visibilityFilter(state = '', action) {
 	switch (action.type) {
@@ -22,16 +22,20 @@ function movies(state = [], action) {
 	}
 }
 
-// function moviesApp(state = {}, action) {
-// 	return {
-// 		visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-// 		movies: movies(state.movies, action),
-// 	};
-// }
+function user(state = [], action) {
+	switch (action.type) {
+		case SET_USER:
+			console.log('user reducer reached');
+			return action.value;
+		default:
+			return state;
+	}
+}
 
 const moviesApp = combineReducers({
 	visibilityFilter,
 	movies,
+	user,
 });
 
 export default moviesApp;
